91625b057078b5ed4387bb9cd053ab190e64e8d1
Daniel Apathy <daniel@apathy.ws>
2022-01-29 17:39:48 +0000

    Add project files.

---
+++ Json.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Simple.Json
+{
+    public static class Json
+    {
+        public static Dictionary<string, string> presets = new Dictionary<string, string>();
+
+        public static void AddPresetKey(string name, string value)
+        {
+            presets.Add(name, value);
+        }
+
+        public static string Escape(string value)
+        {
+            return HttpUtility.JavaScriptStringEncode(value);
+        }
+
+        public static string Convert(object value, int i)
+        {
+            string key = string.Empty;
+            if (i % 2 == 0)
+            {
+                key += "\"" + Escape(value.ToString()) + "\":";
+            }
+            else if (i % 2 == 1)
+            {
+                if (value.GetType() == typeof(int))
+                {
+                    key += value.ToString();
+                }
+                else
+                {
+                    key += "\"" + Escape(value.ToString()) + "\"";
+                }
+
+            }
+            return key;
+        }
+
+        public static string Get(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Get(params object[] values)
+        {
+            string json = string.Empty;
+            for(int i = 0; i < values.Length; i++)
+            {
+                if(values[i].GetType() == new object[] { }.GetType())
+                {
+                    json += Get(values[i] as object[]);
+                } else
+                {
+                    json += Convert(values[i], i);
+                    if (i % 2 == 1)
+                    {
+                        json += ",";
+                    }
+                }
+            }
+
+            if(json.EndsWith(","))
+            {
+                json = json.Substring(0, json.Length - 1);
+            }
+
+            return "{" + json + "}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Json.Get("name", "John \"Test\" Doe", "Age", 35, "Location", new object[] { "Address", "90 Manor Station Street, Zanesville, OH 43701" }));
+            Console.ReadLine();
+        } 
+    }
+}
